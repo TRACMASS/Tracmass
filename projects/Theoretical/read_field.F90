18b673e230b6e105145abfbbe7272fff4656c72e
AitorAldama <aitor.aldama@outlook.com>
2020-03-05 16:27:09 +0100

    Project: Theoretical
    - Simple project that describes an inertial simulation.
    
    - Very simple set up and cheap to run.

---
+++ projects/Theoretical/read_field.F90
@@ -0,0 +1,64 @@
+SUBROUTINE read_field
+
+  !==========================================================================
+  !
+  ! Purpose
+  ! -------
+  !
+  ! Read test model output to advect trajectories.
+  ! Will be called by the loop each time step.
+  !
+  ! Method
+  ! ------
+  !
+  ! Read velocities and optionally some tracers from netCDF files and
+  ! update velocity fields for TRACMASS.
+  !
+  ! Updates the variables:
+  !   uflux and vflux
+  ! ==========================================================================
+
+      USE mod_vel
+      USE mod_grid
+      USE mod_time
+      USE mod_param
+
+      IMPLICIT NONE
+
+      REAL(DP)    :: ug, u0, fcor, gamma, gammag, omtime
+
+      INTEGER     :: jj, ii
+
+      ! Parameters for Nicoletta Fabboni velocities.
+      ug     = 0.04
+      u0     = 0.5
+      fcor   = 2. * 2. * pi/(24.*3600.) * COS(45.*radian)
+      gamma  = 1./(2.89*24.*3600.)
+      gammag = 1./(28.9*24.*3600.)
+      omtime = nff * DBLE(ints) * DBLE(ngcm*3600)
+
+      DO jj=1,JMT
+          DO ii=1,IMT
+
+             IF (ints == 1) THEN
+                   uflux(ii,jj,1,1) = dyu(ii,jj) * dzt(ii,jj,1,1) * u0
+                   vflux(ii,jj,1,1) = 0.d0
+
+             ELSE
+                   uflux(ii,jj,1,1) = uflux(ii,jj,1,2)
+                   vflux(ii,jj,1,1) = vflux(ii,jj,1,2)
+
+             END IF
+
+             uflux(ii,jj,1,2) = dyu(ii,jj) * dzt(ii,jj,1,2) * ( ug*DEXP(-gammag*omtime) + &
+                                                   (u0-ug) * DEXP(-gamma*omtime) * COS(fcor*omtime) )
+             vflux(ii,jj,1,2) = dxv(ii,jj) * dzt(ii,jj,1,2) * ( -(u0-ug) * DEXP(-gamma*omtime) * SIN(fcor*omtime) )
+
+          END DO
+      END DO
+
+      ! zero at north and south boundaries
+      vflux(:,0  ,:,:) = 0.d0
+      vflux(:,JMT,:,:) = 0.d0
+
+END SUBROUTINE read_field
---
+++ projects/Theoretical/setup_grid.F90
@@ -0,0 +1,41 @@
+SUBROUTINE setup_grid
+  ! =============================================================
+  ! Set up the grid
+  ! =============================================================
+  ! Subroutine for defining the grid of the GCM. Run once
+  ! before the loop starts.
+  ! -------------------------------------------------------------
+  ! The following arrays has to be populated:
+  !
+  !  dxdy - Area of horizontal cell-walls.
+  !  dz   - Height of k-cells in 3 dim (if z_timevar not used)
+  !  dzt  - Height of k-cells in 4 dim (if z_timevar used)
+  !  kmt  - Number of k-cells from surface to seafloor.
+  !
+  ! The following might be needed to calculate
+  ! dxdy, uflux, and vflux
+  !
+  !  dzu - Height of each u-gridcell.
+  !  dzv - Height of each v-gridcell.
+  !  dxv -
+  !  dyu -
+  ! -------------------------------------------------------------
+
+  USE mod_grid
+
+  IMPLICIT NONE
+
+  ! Dx and Dy in [m]
+  dxv(:,:) = 250.d0
+  dyu(:,:) = 250.d0
+
+  ! Vertical resolution
+  dzt(:,:,:,:) = 10.d0
+
+  ! Total area
+  dxdy(:,:) = dxv(:,:)*dyu(:,:)
+
+  ! Mask
+  mask(:,:) = 1
+
+END SUBROUTINE setup_grid
